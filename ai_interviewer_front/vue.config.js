f944eab5b1cc1ad559c8ca937b99d865fc2e3782
lihao <15662029002@163.com>
2024-04-08 12:54:20 +0800

    李昊测试

--- ai_interviewer_front/vue.config.js
+++ ai_interviewer_front/vue.config.js
@@ -7,12 +7,13 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:9999',
+        target: 'http://57.180.251.211:81',
         changeOrigin: true,
         pathRewrite: {
-          '^/api': '',  // 将 /api 前缀重写为空，即去掉 /api
+          //'^/api': '',  // 将 /api 前缀重写为空，即去掉 /api
         },
       },
     },
   },
 };
+//lihao测试

40954640b2917657f7b5a1677f91ed94b94f09e5
zhubowen <18066085270@163.com>
2024-04-08 13:25:35 +0900

    修改配置文件

--- ai_interviewer_front/vue.config.js
+++ ai_interviewer_front/vue.config.js
@@ -7,10 +7,10 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://192.268.1.161:9999',
+        target: 'http://57.180.251.211:9999',
         changeOrigin: true,
         pathRewrite: {
-          '^/api': '',  // 将 /api 前缀重写为空，即去掉 /api
+          //'^/api': '',  // 将 /api 前缀重写为空，即去掉 /api
         },
       },
     },

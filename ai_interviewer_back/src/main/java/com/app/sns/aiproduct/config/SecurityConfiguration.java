5a50d9452a1c43e92c0bcc04791c5a38a7fc90de
zhubowen <18066085270@163.com>
2024-04-15 17:49:40 +0900

    フォーマット改修

--- ai_interviewer_back/src/main/java/com/app/sns/aiproduct/config/SecurityConfiguration.java
+++ ai_interviewer_back/src/main/java/com/app/sns/aiproduct/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.app.sns.aiproduct.config;
 
+import com.app.sns.aiproduct.constant.DataDictionary;
 import com.app.sns.aiproduct.handler.MyAccessDeniedHandler;
 import com.app.sns.aiproduct.interceptor.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
@@ -75,20 +76,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 // 指定されたパス下のリソースへのアクセスには検証が必要です
                 .antMatchers("/").permitAll()
                 // ログインアドレスの設定
-                .antMatchers(HttpMethod.POST, "/snsUser/updatePassword").hasAnyRole("2")
-                .antMatchers(HttpMethod.POST, "/snsUser/list").hasAnyRole("0","1")
-                .antMatchers(HttpMethod.POST, "/snsUser/create").hasAnyRole("0","1")
-                .antMatchers(HttpMethod.POST, "/snsUser/update").hasAnyRole("0","1")
-                .antMatchers(HttpMethod.DELETE, "/snsUser/delete/{id}").hasAnyRole("0", "1")
-                .antMatchers(HttpMethod.POST, "/interviewerInfo/batchCreate").hasAnyRole("2")
-                .antMatchers(HttpMethod.POST, "/interviewerInfo/list").hasAnyRole("2")
-                .antMatchers(HttpMethod.POST, "/interviewerInfo/updateInterviewerInfo").hasAnyRole("2")
-                .antMatchers(HttpMethod.POST, "/interviewerInfo/completeInterviewerInfo").hasAnyRole("3")
-                .antMatchers(HttpMethod.GET, "/interviewerInfo/downLoadCsv/{fileId}").hasAnyRole("2")
-                .antMatchers(HttpMethod.POST, "/chat/sendMessage").hasAnyRole("3")
-                .antMatchers(HttpMethod.POST, "/chat/sendMessageByGoogleCloud").hasAnyRole("3")
-                .antMatchers(HttpMethod.POST, "/chat/sendContentByGoogleCloud").hasAnyRole("3")
-                .antMatchers(HttpMethod.POST, "/upload").hasAnyRole("3")
+                .antMatchers(HttpMethod.POST, "/snsUser/updatePassword").hasAnyRole(DataDictionary.ROLE_CONTRACT.getValue())
+                .antMatchers(HttpMethod.POST, "/snsUser/list").hasAnyRole(DataDictionary.ROLE_ADMINISTRATOR.getValue(),DataDictionary.ROLE_STAFF.getValue())
+                .antMatchers(HttpMethod.POST, "/snsUser/create").hasAnyRole(DataDictionary.ROLE_ADMINISTRATOR.getValue(),DataDictionary.ROLE_STAFF.getValue())
+                .antMatchers(HttpMethod.POST, "/snsUser/update").hasAnyRole(DataDictionary.ROLE_ADMINISTRATOR.getValue(),DataDictionary.ROLE_STAFF.getValue())
+                .antMatchers(HttpMethod.DELETE, "/snsUser/delete/{id}").hasAnyRole(DataDictionary.ROLE_ADMINISTRATOR.getValue(),DataDictionary.ROLE_STAFF.getValue())
+                .antMatchers(HttpMethod.POST, "/interviewerInfo/batchCreate").hasAnyRole(DataDictionary.ROLE_CONTRACT.getValue())
+                .antMatchers(HttpMethod.POST, "/interviewerInfo/list").hasAnyRole(DataDictionary.ROLE_CONTRACT.getValue())
+                .antMatchers(HttpMethod.POST, "/interviewerInfo/updateInterviewerInfo").hasAnyRole(DataDictionary.ROLE_CONTRACT.getValue())
+                .antMatchers(HttpMethod.POST, "/interviewerInfo/completeInterviewerInfo").hasAnyRole(DataDictionary.ROLE_INTERVIEWERS.getValue())
+                .antMatchers(HttpMethod.GET, "/interviewerInfo/downLoadCsv/{fileId}").hasAnyRole(DataDictionary.ROLE_CONTRACT.getValue())
+                .antMatchers(HttpMethod.POST, "/chat/sendMessage").hasAnyRole(DataDictionary.ROLE_INTERVIEWERS.getValue())
+                .antMatchers(HttpMethod.POST, "/chat/sendMessageByGoogleCloud").hasAnyRole(DataDictionary.ROLE_INTERVIEWERS.getValue())
+                .antMatchers(HttpMethod.POST, "/chat/sendContentByGoogleCloud").hasAnyRole(DataDictionary.ROLE_INTERVIEWERS.getValue())
+                .antMatchers(HttpMethod.POST, "/upload").hasAnyRole(DataDictionary.ROLE_INTERVIEWERS.getValue())
                 // 他のリクエストには検証が必要
                 .anyRequest().authenticated()
                 .and()
